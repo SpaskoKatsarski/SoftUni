6412f9fa653c6181dbba0eda36330b6533d25e29
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-09-14 17:43:47 +0300

    Delete JavaScript-Advanced-September-2022/Syntax,FunctionsAndStatements-Exercise directory

--- JavaScript-Advanced-September-2022/Syntax,FunctionsAndStatements-Exercise/T01.Fruit.js
+++
@@ -1,7 +0,0 @@
-function calculatePrice(fruit, grams, moneyPerKilogram) {
-    let kilos = grams / 1000;
-
-    console.log(`I need $${(kilos * moneyPerKilogram).toFixed(2)} to buy ${kilos.toFixed(2)} kilograms ${fruit}.`);
-}
-
-calculatePrice('orange', 2500, 1.80);
