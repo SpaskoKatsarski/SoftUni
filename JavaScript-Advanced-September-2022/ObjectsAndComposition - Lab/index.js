7a6b0d3680091f768c44017b717b7031bbd931f1
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-09-20 00:58:29 +0300

    Demo

---
+++ JavaScript-Advanced-September-2022/ObjectsAndComposition - Lab/index.js
@@ -0,0 +1,76 @@
+// let person = {
+//     name: 'Ivan'
+// }
+
+// person.age = 5;
+
+// console.log(person);
+
+// let arr = [1, 2, 3, 4, 5];
+// let [a, b, c, ...rest] = arr;
+// console.log(rest);
+
+
+// let student = {
+//     name: 'Ivan',
+//     age: 15
+// }
+
+// let { age, name } = student;
+// console.log(age);
+
+// student['age']++;
+// console.log(student.age);
+
+// student['city'] = 'Sofia';
+// console.log(student);
+
+// delete student.city;
+// console.log(student)
+
+// class Cat {
+//     name;
+//     age;
+// }
+
+// let cat = new Cat();
+
+// cat.name = 'Sharo';
+// cat.coutnry = 'Bulgaria';
+
+// console.log(cat);
+
+let someCat = {
+    name: 'Ivan',
+    age: 5,
+    owner: {
+        name: 'Pesho',
+        adress: 'Ilinden'
+    }
+}
+
+let copyCat = {...someCat};
+console.log(copyCat);
+
+someCat.owner.name = 'Dido';
+console.log(copyCat.owner.name); // Shallow copy
+
+someCat.name = 'Fiora';
+console.log(someCat.name);
+console.log(copyCat.name);
+
+function canPrint(obj) {
+    let func = () => {
+        return `${this.name} is printing...`
+    }
+
+    obj.print = func;
+}
+
+let printer = {
+    name: 'Cool printer'
+}
+
+canPrint(printer);
+
+console.log(printer.print) // ????
