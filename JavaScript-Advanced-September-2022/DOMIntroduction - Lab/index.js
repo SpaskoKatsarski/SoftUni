d0a9ce19ecddf85a0f8ccc07fda18106ba7a89e5
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-09-22 22:53:19 +0300

    Read more button

---
+++ JavaScript-Advanced-September-2022/DOMIntroduction - Lab/index.js
@@ -0,0 +1,8 @@
+function showFullText() {
+    let fullTextElement = document.getElementsByClassName('full')[0];
+    let smallTextElement = document.getElementsByClassName('small')[0];
+
+    fullTextElement.style.display = 'block';
+    smallTextElement.style.display = 'none';
+}
+
