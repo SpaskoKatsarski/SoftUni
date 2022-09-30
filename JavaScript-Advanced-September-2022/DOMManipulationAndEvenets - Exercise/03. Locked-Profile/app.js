7fbe2e3f9716c6d07f9e7340efc4bd2743b29dca
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-09-30 17:05:35 +0300

    Show and Hide option for profile info

---
+++ JavaScript-Advanced-September-2022/DOMManipulationAndEvenets - Exercise/03. Locked-Profile/app.js
@@ -0,0 +1,23 @@
+function lockedProfile() {
+    let buttons = document.getElementsByTagName('button');
+    
+    for (const button of buttons) {
+        button.addEventListener('click', show);
+    }
+
+    function show(e) {
+        let marks = e.target.parentElement.querySelectorAll('input[type=radio]');
+
+        let unlock = marks[1];
+        debugger;
+        if (unlock.checked) {
+            let id = e.target.parentElement.getElementsByTagName('div')[0].id;
+
+            let condition = document.getElementById(id).style.display = 'none' ? 'block' : 'none'; 
+            document.getElementById(id).style.display = condition;
+
+            let btnText = condition === 'none' ? 'Show more' : 'Hide it';
+            e.target.textContent = btnText;
+        }
+    }
+}
