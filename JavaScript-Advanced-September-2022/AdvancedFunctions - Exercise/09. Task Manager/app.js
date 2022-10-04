c5cd4f8f8d088c17bb783fd55a37ab8f911f2c08
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-10-04 22:32:49 +0300

    Updated Task Manager

--- JavaScript-Advanced-September-2022/AdvancedFunctions - Exercise/09. Task Manager/app.js
+++ JavaScript-Advanced-September-2022/AdvancedFunctions - Exercise/09. Task Manager/app.js
@@ -1,43 +1,82 @@
 function solve() {
-    let addBtn = document.getElementById('add');
+    const inputTask = document.getElementById('task');
+    const inputDescription = document.getElementById('description');
+    const inputDate = document.getElementById('date');
+    const addButton = document.getElementById('add');
+ 
+    let allSections = document.querySelectorAll('section');
+ 
+    let openSection = allSections[1];
+    let progressSection = allSections[2];
+    let completeSection = allSections[3];
+ 
+    addButton.addEventListener('click', addClick);
+ 
+    function clickFirstBtn(e) {
+        let target = e.target;
+        let movedArticle = target.parentElement.parentElement;
+ 
+        if (target.className === "green") {
+            let finishBtn = document.createElement('button');
+            finishBtn.textContent = 'Finish';
+            finishBtn.className = 'orange';
+ 
+            target.remove();
+ 
+            let newArticle = movedArticle.cloneNode(true);
+ 
+            finishBtn.addEventListener('click', clickFirstBtn);
+            newArticle.children[3].appendChild(finishBtn);
+            newArticle.children[3].children[0].addEventListener('click', clickSecondBtn)
+ 
+            movedArticle.remove();
+ 
+            progressSection.children[1].appendChild(newArticle);
+        } else if (target.className === "orange") {
+            movedArticle.children[3].remove();
+            let newArticle = movedArticle.cloneNode(true);
+            movedArticle.remove();
+            completeSection.children[1].appendChild(newArticle);
+        }
+    }
+ 
+    function clickSecondBtn(e) {
+        let movedArticle = e.target.parentElement.parentElement;
+        movedArticle.remove();
+    }
     
-    let startBtn = document.querySelector('#orng-start');
-    let deleteBtn = document.querySelector('#orng-delete');
-
-    addBtn.addEventListener('click', (e) => {
-        let form = e.target.parentElement;
-
-        let taskName = form.children[2].value
-        let description = form.children[6].value
-        let date = form.children[9].value
-
-        if (!taskName || !description || !date) {
+    function addClick(e) {
+        e.preventDefault();
+        let task = inputTask.value;
+        let descr = inputDescription.value;
+        let date = inputDate.value;
+ 
+        if (task === '' || descr === '' || date === '') {
             return;
         }
-        let orangeSection = document.getElementsByClassName('orange')[0].parentElement.parentElement;
-
-        orangeSection.getElementsByTagName('div')[1].innerHTML += '<article>' +
-        `<h3>${taskName}</h3>` +
-        `<p>Description: ${description}</p>` +
-        `<p>Due Date: ${date}</p>` +
-        '<div class="flex">' +
-        '<button class="green" id="orng-start">Start</button>' +
-        '<button class="red" id="orng-delete">Delete</button>' +
-        '</div>' +
-        '</article>'
-        e.preventDefault();
-    });
-    
-    startBtn.addEventListener('click', () => {
-        greenSection.getElementsByTagName('div')[1].innerHTML += '<article>' +
-        `<h3>${taskName}</h3>` +
-        `<p>Description: ${description}</p>` +
-        `<p>Due Date: ${date}</p>` +
-        '<div class="flex">' +
-        '<button class="green">Start</button>' +
-        '<button class="red">Delete</button>' +
-        '</div>' +
-        '</article>'
-    })
-    
+ 
+        let articleElement = document.createElement('article');
+        let divElement = document.createElement('div');
+        divElement.className = "flex";
+ 
+        let startBtn = document.createElement('button');
+        let deleteBtn = document.createElement('button');
+        startBtn.className = "green";
+        deleteBtn.className = "red";
+ 
+        startBtn.textContent = 'Start';
+        deleteBtn.textContent = 'Delete';
+ 
+        startBtn.addEventListener('click', clickFirstBtn);
+        deleteBtn.addEventListener('click', clickSecondBtn);
+ 
+        divElement.appendChild(startBtn);
+        divElement.appendChild(deleteBtn);
+ 
+        articleElement.innerHTML = `<h3>${task}</h3><p>Description: ${descr}</p><p>Due Date: ${date}</p>`;
+ 
+        articleElement.appendChild(divElement);
+ 
+        openSection.children[1].appendChild(articleElement);
+    }
 }
