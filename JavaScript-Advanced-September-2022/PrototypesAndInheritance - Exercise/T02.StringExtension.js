be7ef28e3b9eb9c635b35829ca1dabc62c957e42
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-10-13 18:50:39 +0300

    Updated

--- JavaScript-Advanced-September-2022/PrototypesAndInheritance - Exercise/T02.StringExtension.js
+++ JavaScript-Advanced-September-2022/PrototypesAndInheritance - Exercise/T02.StringExtension.js
@@ -14,11 +14,25 @@
     String.prototype.truncate = function(n) {
         let str = this.toString();
 
-        if (str.length < n) {
+        if (str.length <= n) {
             return str;
         }
 
-        while (str.length > n) {
+        if (n < 4) {
+            let res = '';
+
+            for (let i = n; i > 0; i--) {
+                res += '.';
+            }
+
+            return res;
+        }
+
+        if (!str.includes(' ')) {
+            return str.substring(0, n - 3) + '...';
+        }
+
+        while (str.length + 3 > n) {
             let words = str.split(' ');
 
             words.splice(words.length - 1);
@@ -26,29 +40,47 @@
             str = words.join(' ');
         }
 
-        let result = str + '...';
-
-        if (condition) {
-            
-        }
         return str + '...';
     }
 
-    String.prototype.format = function(string, ...params) {
+    String.format = function(string, ...params) {
+        let result = [];
 
+        let words = string.split(' ');
+
+        for (let word of words) {
+            if (word.startsWith('{') && word.endsWith('}')) {
+                if (params.length > 0) {
+                    word = params.shift();
+                }
+            }
+
+            result.push(word);
+        }
+
+        return result.join(' ');
     }
 })();
 
 let str = 'my string';
+
 str = str.ensureStart('my');
+console.log(str);
 str = str.ensureStart('hello ');
+console.log(str);
 str = str.truncate(16);
+console.log(str);
 str = str.truncate(14);
+console.log(str);
 str = str.truncate(8);
+console.log(str);
 str = str.truncate(4);
+console.log(str);
 str = str.truncate(2);
 console.log(str);
 str = String.format('The {0} {1} fox',
   'quick', 'brown');
+  console.log(str);
 str = String.format('jumps {0} {1}',
   'dog');
+  console.log(str);
