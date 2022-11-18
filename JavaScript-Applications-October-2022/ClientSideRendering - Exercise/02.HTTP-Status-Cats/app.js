7d860cd37c4f4ed6c650a9e93b6a2c3e640306aa
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-11-18 15:50:10 +0200

    HTTP Cats

---
+++ JavaScript-Applications-October-2022/ClientSideRendering - Exercise/02.HTTP-Status-Cats/app.js
@@ -0,0 +1,46 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { cats } from './catSeeder.js';
+
+const root = document.getElementById('allCats');
+
+const template = html`
+    <ul>
+        ${cats.map(createCatCard)}
+    </ul>
+`
+
+loadAllCats();
+
+function loadAllCats() {
+    render(template, root);
+}
+
+function createCatCard(cat) {
+    return html`
+    <li>
+        <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+        <div class="info">
+            <button @click="${showContent}" class="showBtn">Show status code</button>
+            <div class="status" style="display: none" id=${cat.id}>
+                <h4>Status Code: ${cat.statusCode}</h4>
+                <p>${cat.statusMessage}</p>
+            </div>
+        </div>
+    </li>`;
+}
+
+function showContent(e) {
+    debugger;
+    const contentContainer = e.target.parentElement.querySelector('div');
+    const currentState = contentContainer.style.display;
+
+    if (currentState === 'none') {
+        contentContainer.style.display = 'block';
+        e.target.textContent = 'Hide status code';
+    } else {
+        contentContainer.style.display = 'none';
+        e.target.textContent = 'Show status code';
+    }
+
+    loadAllCats();
+}
