b5b6246860862d481c34999978b5599f9c1308a3
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-11-18 14:35:30 +0200

    Presenting a list of towns from a given input

---
+++ JavaScript-Applications-October-2022/ClientSideRendering - Exercise/01.List-Towns/app.js
@@ -0,0 +1,34 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+
+const root = document.getElementById('root');
+const form = document.querySelector('form');
+const btn = document.getElementById('btnLoadTowns');
+
+btn.addEventListener('click', onSubmit);
+
+function onSubmit() {
+    const towns = document.getElementById('towns').value.split(', ')
+
+    if (towns.length === 1 && towns[0] === '') {
+        root.replaceChildren();
+        return;
+    }
+
+    const listOfTowns = html`
+    <ul>
+        ${towns.map(createTownCard)}
+    </ul>
+    `;
+
+    update(listOfTowns);
+
+    form.reset();
+}
+
+function update(items) {
+    render(items, root);
+}
+
+function createTownCard(town) {
+    return html`<li>${town}</li>`;
+}
