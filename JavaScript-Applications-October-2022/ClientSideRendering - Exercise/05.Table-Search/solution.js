53dfdd59b7f4300d1b53755b8ecd26eee939d357
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-11-18 16:40:13 +0200

    Search application

---
+++ JavaScript-Applications-October-2022/ClientSideRendering - Exercise/05.Table-Search/solution.js
@@ -0,0 +1,75 @@
+import { html, render } from './node_modules/lit-html/lit-html.js'
+
+function solve() {
+   const url = 'http://localhost:3030/jsonstore/advanced/table';
+
+   const tableBody = document.querySelector('tbody');
+   const input = document.getElementById('searchField');
+
+   document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+   function onClick() {
+      const text = input.value;
+
+      //add select if includes the string
+      //remove select if it doesnt
+
+      update(text);
+
+      input.value = '';
+   }
+
+   update();
+
+   async function update(text) {
+      const data = await getData();
+
+      const result = data.map(p => createRowCard(p, text));
+
+      render(result, tableBody);
+   }
+
+   function createRowCard(person, match = undefined) {
+      const firstName = person.firstName;
+      const lastName = person.lastName;
+      const email = person.email;
+      const course = person.course;
+
+      if (match === undefined) {
+         return html`
+         <tr>
+            <td>${firstName} ${lastName}</td>
+            <td>${email}</td>
+            <td>${course}</td>
+         </tr>`;
+      }
+
+      if (firstName.includes(match)
+      || lastName.includes(match)
+      || email.includes(match)
+      || course.includes(match)) {
+         return html`
+         <tr class="select">
+            <td>${firstName} ${lastName}</td>
+            <td>${email}</td>
+            <td>${course}</td>
+         </tr>`;
+      }
+
+      return html`
+         <tr>
+            <td>${firstName} ${lastName}</td>
+            <td>${email}</td>
+            <td>${course}</td>
+         </tr>`;
+   }
+
+   async function getData() {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      return Object.values(data);
+   }
+}
+
+solve();
