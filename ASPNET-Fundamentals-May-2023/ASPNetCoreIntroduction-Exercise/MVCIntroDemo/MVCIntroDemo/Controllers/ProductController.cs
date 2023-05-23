40ebea467d6c90340567bf4649c0cab943b5ae7e
Spasko Katsarski <patsokatsarski1@gmail.com>
2023-05-23 18:04:54 +0300

    Web app demo MVC

---
+++ ASPNET-Fundamentals-May-2023/ASPNetCoreIntroduction-Exercise/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿namespace MVCIntroDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using MVCIntroDemo.Models.Product;
+
+    public class ProductController : Controller
+    {
+        private readonly IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 7.00
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.50
+            }
+        };
+
+        public IActionResult All()
+        {
+            return View(products);
+        }
+    }
+}
