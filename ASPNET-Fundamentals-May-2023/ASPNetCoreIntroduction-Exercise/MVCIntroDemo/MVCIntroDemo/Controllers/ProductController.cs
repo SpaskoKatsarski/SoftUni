4c503c3f10dfbff36ca08e7ba579e93c70b4a09c
Spasko Katsarski <patsokatsarski1@gmail.com>
2023-05-23 21:47:00 +0300

    Finished

--- ASPNET-Fundamentals-May-2023/ASPNetCoreIntroduction-Exercise/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
+++ ASPNET-Fundamentals-May-2023/ASPNetCoreIntroduction-Exercise/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
@@ -1,6 +1,11 @@
 ﻿namespace MVCIntroDemo.Controllers
 {
+    using System.Diagnostics;
+    using System.Text;
+    using System.Text.Json;
+
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Net.Http.Headers;
 
     using MVCIntroDemo.Models.Product;
 
@@ -28,9 +33,70 @@
             }
         };
 
-        public IActionResult All()
+        [ActionName("My-Products")]
+        public IActionResult All(string keyword)
         {
-            return View(products);
+            if (keyword != null)
+            {
+                var correspondingProducts = this.products.Where(p => p.Name.ToLower().Contains(keyword.ToLower()));
+
+                if (!correspondingProducts.Any())
+                {
+                    return BadRequest();
+                }
+
+                return View(correspondingProducts);
+            }
+
+            return View(this.products);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var productModel = this.products.FirstOrDefault(p => p.Id == id);
+
+            if (productModel == null)
+            {
+                return BadRequest();
+            }
+
+            return View(productModel);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            return Json(this.products, options);
+        }
+
+        public IActionResult AllAsText()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var product in this.products)
+            {
+                sb.AppendLine($"Product {product.Id}: {product.Name} - {product.Price:f2} lv.");
+            }
+
+            return Content(sb.ToString().TrimEnd());
+        }
+
+        public IActionResult AllAsTextFile()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var product in this.products)
+            {
+                sb.AppendLine($"Product {product.Id}: {product.Name} - {product.Price:f2} lv.");
+            }
+
+            Response.Headers.Add(HeaderNames.ContentDisposition, @"attachment;filename=products.txt");
+
+            return File(Encoding.UTF8.GetBytes(sb.ToString().TrimEnd()), "text/plain");
         }
     }
 }
