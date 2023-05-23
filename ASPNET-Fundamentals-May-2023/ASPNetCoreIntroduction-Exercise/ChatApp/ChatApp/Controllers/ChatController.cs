89e8ce1c52ec1381eb44c25a10d1b76d61422123
Spasko Katsarski <patsokatsarski1@gmail.com>
2023-05-23 22:09:07 +0300

    Making a chat application

---
+++ ASPNET-Fundamentals-May-2023/ASPNetCoreIntroduction-Exercise/ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ChatApp.Models.Message;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly ICollection<KeyValuePair<string, string>> messages = 
+            new List<KeyValuePair<string, string>>();
+
+        public IActionResult Show()
+        {
+            if (this.messages.Count < 1)
+            {
+                return View(new ChatViewModel());
+            }
+
+            //TODO: implement the logic
+            var chatModel = new ChatViewModel()
+            {
+                
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Send()
+        {
+
+        }
+    }
+}
