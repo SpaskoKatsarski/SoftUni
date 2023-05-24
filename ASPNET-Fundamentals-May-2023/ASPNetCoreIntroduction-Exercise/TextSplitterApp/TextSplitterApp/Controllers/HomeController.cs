9a6b5d475e785111ecc5764deafb84d6a1cacec2
Spasko Katsarski <patsokatsarski1@gmail.com>
2023-05-24 16:10:51 +0300

    Making a simple text splitter app

---
+++ ASPNET-Fundamentals-May-2023/ASPNetCoreIntroduction-Exercise/TextSplitterApp/TextSplitterApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextSplitterApp.Models;
+
+namespace TextSplitterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextViewModel model)
+            => View(model);
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var splitText = model.Text
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            model.SplitText = String.Join(Environment.NewLine, splitText);
+
+            return RedirectToAction("Index", model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASPNET-Fundamentals-May-2023/ASPNetCoreIntroduction-Exercise/TextSplitterApp/TextSplitterApp/Models/TextViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TextSplitterApp.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class TextViewModel
+    {
+        [Required(ErrorMessage = "The text field is required.")]
+        [StringLength(30, MinimumLength = 2)]
+        public string Text { get; set; } = null!;
+
+        public string SplitText { get; set; } = null!;
+    }
+}
